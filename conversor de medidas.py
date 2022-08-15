833e247e3d6471f91c4f01944d67c00b0cd438e2
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2022-08-15 17:37:58 -0300

    new code
    novo code

---
+++ conversor de medidas.py
@@ -0,0 +1,5 @@
+n1 = float(input ('Digite um numero em km: '))
+
+print ('{} km é igual a {} metros'.format((n1), (n1 * 1000))) 
+print ('{} km é igual a {} centimetros'.format((n1), (n1 * 100000)))
+
