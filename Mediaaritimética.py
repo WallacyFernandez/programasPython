2fdc769bec3dee4d16304719699013d51c393bea
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2022-08-15 17:27:00 -0300

    fix bugs
    alterado.

---
+++ Mediaaritimética.py
@@ -0,0 +1,7 @@
+n1 = float(input ('Digite um numero: '))
+n2 = float(input ('Digite um numero: '))
+n3 = float(input ('Digite um numero: '))
+ 
+n4 = ( n1 + n2 + n3)/3
+
+print ("A media aritmetica = {}".format (n4))
---
+++ exercicio.py
@@ -0,0 +1,17 @@
+n1 = int(input ('Digite um numero: '))
+n2 = int(input ('Digite um segundo numero: '))
+
+s =  n1+n2
+print("\nO valor da soma é: {}".format(s))
+
+n3 = int(input ('Digite um numero: '))
+
+print('\nO sucessor do numero digitado é: {} e o antecessor é:{}'.format((n3 + 1), (n3 - 1)))
+
+print(' O numero elevado ao cubo é = {}'.format(n3 ** 3))
+print(' O numero elevado ao Quandrado é = {}'.format(n3 ** 2))
+print(' A raiz quadrada do numero é = {:.2f}'.format(n3 ** (1/2)))
+print(' O dobro do numero = {}'.format(n3 * 2))
+print(' O triplo do numero = {}'.format(n3 * 3))
+
+
